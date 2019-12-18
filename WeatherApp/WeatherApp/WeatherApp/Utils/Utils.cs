4ed88a3ba48c087f657cc1df2c668f7a500e5a12
António Pereira <antonio_m_sp@hotmail.com>
2019-12-18 14:54:12 +0000

    Start getbackground icons utils function.

---
+++ WeatherApp/WeatherApp/WeatherApp/Utils/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Utils
+{
+    public static class Utils
+    {
+        public static string retrieveWeatherBackgroundIcon(string descrip)
+        {
+            switch(descrip)
+            {
+
+            }
+
+            return "";
+        }
+    }
+}
