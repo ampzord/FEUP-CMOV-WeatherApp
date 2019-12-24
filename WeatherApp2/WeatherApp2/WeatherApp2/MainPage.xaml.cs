d2fa5426c05c1e2b9480f499e0f9646bb3a90f90
António Pereira <antonio_m_sp@hotmail.com>
2019-12-24 03:05:04 +0000

    clean folders, fix a bit layout.

---
+++ WeatherApp2/WeatherApp2/WeatherApp2/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using WeatherApp2.Utils;
+using Xamarin.Forms;
+using WeatherApp2.Model;
+
+
+namespace WeatherApp2
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        private List<string> city_districts_pt =
+                new List<string>() {
+                "Leiria", "Porto", "Viana do Castelo", "Braga", "Vila Real", "Bragança", "Aveiro", "Viseu", "Guarda",
+                "Coimbra", "Castelo Branco", "Santarém", "Castelo Branco", "Lisboa", "Portalegre",
+                "Évora", "Setúbal", "Beja", "Faro"
+        };
+
+        //private List<string> favorite_cities;
+
+        private WeatherRootObject weather_object;
+        private ForecastRootObject forecast_object;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InititializeProgram();
+        }
+
+        private async void InititializeProgram()
+        {
+            DevicePlatform();
+            Title = city_districts_pt[0] + ", Portugal";
+            addCity.ItemsSource = city_districts_pt;
+            addCity.SelectedItem = city_districts_pt[0];
+            BindingContext = this;
+            if (NetworkCheck.IsInternetAsync())
+            {
+                weather_object = await APIRequest.GetJSONWeatherRequest(city_districts_pt[0]);
+                forecast_object = await APIRequest.GetJSONForecastRequest(city_districts_pt[0]);
+                if (weather_object == null || forecast_object == null)
+                {
+                    await DisplayAlert("API Error", "Error doing request.", "Ok");
+                }
+
+                BindingWeatherInfo();
+                IsPresented = false;
+            }
+            else
+            {
+                await DisplayAlert("JSONParsing", "No network is available.", "Ok");
+            }
+        }
+
+        private void DevicePlatform()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+            /*else if (Device.RuntimePlatform == Device.Android)
+                Padding = new Thickness(0, 20, 0, 0);
+            else if (Device.RuntimePlatform == Device.UWP)
+                Padding = new Thickness(0, 20, 0, 0);*/
+        }
+
+        private async void AddCitySelectedIndexChanged(object sender, EventArgs e)
+        {
+            Picker picker = addCity;
+            var selectedItem = (string)picker.SelectedItem;
+
+            if (!selectedItem.Equals(city_districts_pt[0]))
+            {
+                if (NetworkCheck.IsInternetAsync())
+                {
+                    weather_object = await APIRequest.GetJSONWeatherRequest(selectedItem);
+                    forecast_object = await APIRequest.GetJSONForecastRequest(selectedItem);
+                    if (weather_object == null || forecast_object == null)
+                    {
+                        await DisplayAlert("API Error", "Error doing request.", "Ok");
+                    }
+
+                    BindingWeatherInfo();
+                    Title = this.weather_object.name + ", Portugal";
+                }
+                else
+                {
+                    await DisplayAlert("JSONParsing", "No network is available.", "Ok");
+                }
+
+
+                //string asd = Utils.Utils.hourIcon("");
+                //Debug.WriteLine("day: " + asd);
+                //Utils.Utils.setFavoriteCities(selectedItem);
+                //string savedCity = Utils.Utils.getFavoriteCities();
+                //Debug.WriteLine("City saved to EditPreferences: " + savedCity);
+
+            }
+            IsPresented = false;
+        }
+
+        protected async void ChartForecast_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ForecastChart(forecast_object));
+        }
+
+        protected async void Forecast_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Forecast(forecast_object));
+        }
+
+        private void BindingWeatherInfo()
+        {
+            //CurrentCity.Text = this.weather_object.name + ", Portugal";
+            CurrentTemp.Text = this.weather_object.main.temp.ToString("0");
+            CurrentDescription1.Text = Utils.Utils.FirstLetterToUpper(this.weather_object.weather[0].description);
+            CurrentDescription2.Text = DateTime.Now.ToString("ddd, dd MMM HH:mm");
+            CurrentHumidity.Text = this.weather_object.main.humidity.ToString() + "%";
+            CurrentPressure.Text = this.weather_object.main.pressure.ToString() + " hPa";
+            double newSpeed = this.weather_object.wind.speed * 3.6;
+
+            CurrentWind.Text = newSpeed.ToString("0") + " km/h";
+            CurrentCloudiness.Text = this.weather_object.clouds.all.ToString() + "%";
+            CurrentFeelsLike.Text = this.weather_object.main.feels_like.ToString("0") + " ºC";
+            CurrentTempMax.Text = this.weather_object.main.temp_max.ToString("0") + " ºC";
+            CurrentTempMin.Text = this.weather_object.main.temp_min.ToString("0") + " ºC";
+
+            //Title = 
+
+            if (Utils.Utils.hourIcon().Equals("day"))
+            {
+                string url = Utils.Utils.retrieveWeatherBackgroundIconDay(this.weather_object.weather[0].main, this.weather_object.weather[0].description);
+                WeatherIcon.Source = url;
+            }
+            else
+            {
+                string url = Utils.Utils.retrieveWeatherBackgroundIconNight(this.weather_object.weather[0].main, this.weather_object.weather[0].description);
+                WeatherIcon.Source = url;
+            }
+
+        }
+
+        private void OnMenuClicked(object sender, EventArgs args)
+        {
+            try
+            {
+                IsPresented = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
---
+++ WeatherApp2/WeatherApp2/WeatherApp2/Utils/Utils.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Essentials;
+using System.Diagnostics;
+
+namespace WeatherApp2.Utils
+{
+    public static class Utils
+    {
+        public static string retrieveWeatherBackgroundIconDay(string main_description, string description)
+        {
+            switch (main_description)
+            {
+                case "Clear":
+                    return "clear_sky.png";
+                case "Clouds":
+                    if (description.Equals("few clouds"))
+                    {
+                        return "few_clouds.png";
+                    }
+                    else if (description.Equals("scattered clouds"))
+                    {
+                        return "scattered_clouds.png";
+                    }
+                    else if (description.Equals("broken clouds"))
+                    {
+                        return "broken_clouds.png";
+                    }
+                    else
+                    {
+                        return "broken_clouds.png";
+                    }
+                case "Snow":
+                    return "snow.png";
+                case "Rain":
+                    if (description.Equals("light rain") || description.Equals("moderate rain") || description.Equals("heavy intensity rain"))
+                    {
+                        Debug.Write("Ola");
+                        return "rain.png";
+                    }
+                    else if (description.Equals("freezing rain"))
+                    {
+                        return "snow.png";
+                    }
+                    else
+                    {
+                        return "shower_rain.png";
+                    }
+                case "Drizzle":
+                    return "shower_rain.png";
+                case "Thunderstorm":
+                    return "thunderstorm.png";
+                case "Mist":
+                    return "mist.png";
+                default:
+                    return "mist.png";
+
+            }
+        }
+
+        public static string retrieveWeatherBackgroundIconNight(string main_description, string description)
+        {
+            switch (main_description)
+            {
+                case "Clear":
+                    return "clear_sky_n.png";
+                case "Clouds":
+                    if (description.Equals("few clouds"))
+                    {
+                        return "few_clouds_n.png";
+                    }
+                    else if (description.Equals("scattered clouds"))
+                    {
+                        return "scattered_clouds_n.png";
+                    }
+                    else if (description.Equals("broken clouds"))
+                    {
+                        return "broken_clouds_n.png";
+                    }
+                    else
+                    {
+                        return "rain_n.png"; // return "broken_clouds_n.png";
+                    }
+                case "Snow":
+                    return "snow_n.png";
+                case "Rain":
+                    if (description.Equals("light rain") || description.Equals("moderate rain") || description.Equals("heavy intensity rain"))
+                    {
+
+                        return "rain_n.png";
+                    }
+                    else if (description.Equals("freezing rain"))
+                    {
+                        return "snow_n.png";
+                    }
+                    else
+                    {
+                        return "shower_rain_n.png";
+                    }
+                case "Drizzle":
+                    return "shower_rain_n.png";
+                case "Thunderstorm":
+                    return "thunderstorm_n.png";
+                case "Mist":
+                    return "mist_n.png";
+                default:
+                    return "mist_n.png";
+
+            }
+        }
+
+        public static string getFavoriteCities()
+        {
+            return Preferences.Get("FavCities", "");
+        }
+
+        public static void setFavoriteCities(string value)
+        {
+            string prevValue = Preferences.Get("FavCities", "");
+
+            if (prevValue == "")
+            {
+                Preferences.Set("FavCities", value);
+            }
+            else
+            {
+                string all_fav_cities = prevValue + "," + value;
+                Preferences.Set("FavCities", all_fav_cities);
+            }
+
+        }
+
+        //toRemove() - Preferences.Remove("my_key");
+        //toRemoveAll() - Preferences.Clear();
+
+        //working
+        public static string hourIcon()
+        {
+            DateTime localDate = DateTime.Now;
+            Debug.WriteLine("hora: " + localDate.Hour.ToString());
+
+            int value = Convert.ToInt32(localDate.Hour.ToString());
+
+            if (value >= 0 && value <= 6)
+            {
+                return "night";
+            }
+            else if (value >= 7 && value <= 18)
+            {
+                return "day";
+            }
+            else
+                return "night";
+        }
+
+        public static string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+    }
+}
